b2d064eb32a57dcbf38430b8db5f1f3c723a0022
wesling18 <bermudezwesling@gmail.com>
2026-03-23 23:49:16 -0600

    Nuevos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Encabezado from "./components/navegacion/Encabezado";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Inicio from "./views/Inicio";
+import Categorias from "./views/Categorias";
+import Catalogo from "./views/Catalogo";
+import Login from "./views/Login";
 
+import RutaProtegida from "./components/rutas/RutaProtegida";
+import Pagina404 from "./views/Pagina404";
+
+import "./App.css";
+
+
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      
+      <Encabezado />
+
+      <main className="margen-superior-main">
+        <Routes>
+
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/" element={<RutaProtegida><Inicio /></RutaProtegida>} />
+          <Route path="/categorias" element={<RutaProtegida><Categorias /></RutaProtegida>} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/productos" element={<RutaProtegida><Productos /></RutaProtegida>} />
+
+          <Route path="*" element={<Pagina404 />} />
+
+        </Routes>
+      </main>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/assets/views/Pagina404.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+
+const Pagina404 = () => {
+  return (
+    <Container className="mt-3">
+      <Row className="align-items-center">
+        <Col>
+          <h2>
+            <i className="bi-house-fill me-2"></i> Pagina404
+          </h2>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Pagina404;
